aa002d94809cfa90af92ff537fcb2d2e149d1461
Christian Johansen <137861@RTS-365.dk>
2020-10-21 11:46:04 +0200

    ny CP Button

--- src/Components/ControlPanelButton.js
+++ src/Components/ControlPanelButton.js
@@ -3,14 +3,25 @@ import { css, jsx } from "@emotion/core";
 
 const ControlPanelButton = ({label, pictogram, no }) => {
 
-
-
+    const style = css`
+    width: 9.375em;
+    height: 9.375em;
+    border: 10px solid black;
+    background-color:#FFFFFF;
+    color:#002D67;
+    font-size: 1.1875em;
+    font-weight: bold;
+    border-radius: 1em;
+    border-style:none;
+    font-family: 'Montserrat', sans-serif;
+    
+`;
 
 
     return ( 
 
 
-        <button 
+        <button css={style}
         type="button"
         className={}
         img={pictogram}
