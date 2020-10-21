0be69ad1693337f1f5050016da1d69948e00027b
Christian Johansen <137861@RTS-365.dk>
2020-10-21 09:25:07 +0200

    ControlPanelButton

---
+++ src/Components/ControlPanelButton.js
@@ -0,0 +1,25 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const ControlPanelButton = ({label, pictogram, no }) => {
+
+
+
+
+
+    return ( 
+
+
+        <button 
+        type="button"
+        className={}
+        img={pictogram}
+        text={no}
+        {...props}
+        
+        >{label}</button>
+
+     );
+}
+ 
+export default ControlPanelButton;
