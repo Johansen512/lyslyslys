1b1a038dd0dba18ff7134803dcd622cc89a41423
Johansen512 <tekstogtext@gmail.com>
2020-10-22 10:32:28 +0200

    CPB styling

--- src/Components/ControlPanelButton.js
+++ src/Components/ControlPanelButton.js
@@ -4,14 +4,17 @@ import { css, jsx } from "@emotion/core";
 const ControlPanelButton = ({label, pictogram, no, props }) => {
 
     const style = css`
+    display: flex;
+    flex-direction: column;
+    padding: 0.5em;
+    margin: 0.75em;
     width: 9.375em;
     height: 9.375em;
-    border: 10px solid black;
     background-color:#FFFFFF;
     color:#002D67;
     font-size: 1.1875em;
     font-weight: bold;
-    border-radius: 1em;
+    border-radius: 1.5em;
     border-style:none;
     font-family: 'Montserrat', sans-serif;
     
