913e80d9480600dc3395a5c8a43d1fea1767eb2c
Johansen512 <tekstogtext@gmail.com>
2020-10-22 13:22:36 +0200

    nu med sider ...

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import SwitchButton from "./Components/SwitchButton";
 import Allrooms from "../src/Components/AllRooms"
+import Controlpanel from "../src/View/Controlpanel";
 
 function App() {
   return (
     <div className="App">
+      <Controlpanel />
       <Allrooms />
 
         
---
+++ src/View/Controlpanel.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const Controlpanel = () => {
+    return ( 
+
+        <div>Testing testing</div>
+     );
+}
+ 
+export default Controlpanel;
