ca4e51f3a4697ec1a63dbce9fec03d2c9536179a
Johansen512 <tekstogtext@gmail.com>
2020-10-26 09:21:10 +0100

    roomheader is ahead

--- src/Components/Roomheader.js
+++ src/Components/Roomheader.js
@@ -7,10 +7,10 @@ const Roomheader = ({room, no}) => {
 
     const imgstyle = css`
     
-    width:3.3125rem;
-    height:3.3125rem;
-    border-radius:3.125rem;
-    border: solid white 0.125rem;
+    width:0.87875rem;
+    height:0.82875rem;
+   
+    
     
 `;
 
@@ -27,7 +27,7 @@ align-items: center;
     return (
 
 
-<header css={headerstyle}><img css={imgstyle} src="img/backarrow.png"/><h1>{room}</h1><img css={imgstyle} src="img/user0.jpg"/><p>'${no} lights'</p><RoomlightbuttonCluster /></header>
+<header css={headerstyle}><img css={imgstyle} src="img/backarrow.png"/><h1>{room}</h1><p>`${no} lights`</p><RoomlightbuttonCluster /></header>
 
       );
 }
