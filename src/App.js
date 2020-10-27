8aae24e9014a3f84da67f621ab2a6d7d34f31703
Johansen512 <tekstogtext@gmail.com>
2020-10-27 11:03:10 +0100

    en masse rod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Router } from "@reach/router";
-import SceneButton from "../src/Components/SceneButton";
+import SwitchButton from "./Components/SwitchButton";
 import Allrooms from "../src/Components/AllRooms"
-import Mainview from "../src/View/Mainview";
-import Roomview from "../src/View/Roomview";
-import DataContextProvider from "../src/Contexts/DataContext"
-
-
 
 function App() {
   return (
     <div className="App">
-     
-     <DataContextProvider>
-      <Router>
-                    <Roomview path="/roomview" />
-                    <Mainview path="/" />
-                </Router>
-
-</DataContextProvider>
+      <Allrooms />
 
         
     </div>
