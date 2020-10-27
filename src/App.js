db60108c5c778b986534c690b13c5042ec2df7a6
Johansen512 <tekstogtext@gmail.com>
2020-10-27 09:07:03 +0100

    noget med context

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SceneButton from "../src/Components/SceneButton";
 import Allrooms from "../src/Components/AllRooms"
 import Mainview from "../src/View/Mainview";
 import Roomview from "../src/View/Roomview";
+import DataContextProvider from "../src/Contexts/DataContext"
 
 
 
@@ -12,11 +13,13 @@ function App() {
   return (
     <div className="App">
      
-     
+     <DataContextProvider>
       <Router>
                     <Roomview path="/roomview" />
                     <Mainview path="/" />
                 </Router>
+
+</DataContextProvider>
 
         
     </div>
