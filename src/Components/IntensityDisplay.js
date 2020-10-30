010b08106e7bd7450f9c065a536f3403e05b716b
Johansen512 <tekstogtext@gmail.com>
2020-10-30 11:36:47 +0100

    rå slider

--- src/Components/IntensityDisplay.js
+++ src/Components/IntensityDisplay.js
@@ -1,14 +1,15 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import { useState } from "react";
 
 const IntensityDisplay = ({props}) => {
 
   const style= css`
   display: flex;
   flex-direction:row;
-  justify-content:space-evenly;
+  justify-content: space-between;
   align-items: center;
-  margin: 0 4rem;
+  margin: 0 1rem;
   
   
   
@@ -16,13 +17,16 @@ const IntensityDisplay = ({props}) => {
 
 
 
-const sliderstyle = css({
+const sliderstyle = css`
   
-appearance: 'none',
-backgroundColor: '#CECECE',
-height: '0.1rem',
-padding: '0 4rem',
-});
+appearance: none;
+background-color: #CECECE;
+height: 0.1rem;
+width: 200px;
+margin: 0 1rem;
+
+
+`;
 
 /*const sliderstyleFocus = css({
   
@@ -37,7 +41,7 @@ const imgstyle = css`
 
 
 
-padding: 0 0.5rem;
+padding: 0 0.2rem;
 
 
 
@@ -49,17 +53,18 @@ padding: 0 0.5rem;
 
 `;*/
 
+const [intensity, setIntensity] = useState ("0")
 
 
-
-
+console.log (intensity)
 
 
 
     return ( 
-        
-    <div css={style}><img css={imgstyle} src="../img/bulbOff.png" alt="Off"/><input css={sliderstyle}type="range" min="1" max="200" value="200"/><img css={imgstyle} src="../img/bulbOn.png" alt=""/></div>
-        
+       <div css={style}>
+      <img css={imgstyle} src="../img/bulbOff.png" alt="Light"/><input css={sliderstyle} type="range" min="0" max="100" 
+      value={intensity} onChange={ e => setIntensity (e.target.value)}step="1"/><img css={imgstyle} src="../img/bulbOn.png" alt="Light"/></div>
+      
     );
 }
  
