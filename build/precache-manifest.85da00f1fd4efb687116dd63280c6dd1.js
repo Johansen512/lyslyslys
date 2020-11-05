49db0e85c331555210f6555d2743868d96f9dbd3
Johansen512 <tekstogtext@gmail.com>
2020-11-05 10:35:26 +0100

    piller ved Build

---
+++ build/precache-manifest.85da00f1fd4efb687116dd63280c6dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b1f05f40270f5671a04632f9287d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2061647cf92b1805586e",
+    "url": "/static/css/main.ffbc9bb6.chunk.css"
+  },
+  {
+    "revision": "939b0669c1ae99b20fb6",
+    "url": "/static/js/2.0d9647ec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0d9647ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2061647cf92b1805586e",
+    "url": "/static/js/main.f08b25e0.chunk.js"
+  },
+  {
+    "revision": "a8c9bec0b8b1ea488715",
+    "url": "/static/js/runtime-main.47678e2a.js"
+  }
+]);
